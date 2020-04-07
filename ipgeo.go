4fbeedeb88297b68069869baea45195ba556f74a
kamingchan <hi@terrychan.me>
2020-04-07 23:56:34 +0800

    feat: use private api

--- ipgeo.go
+++ ipgeo.go
@@ -3,7 +3,9 @@ package ipipgo
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net"
+	"strconv"
 	"strings"
 )
 
@@ -13,7 +15,7 @@ var (
 )
 
 const (
-	responseLen = 5
+	responseLen = 7
 )
 
 type IPGeo struct {
@@ -24,6 +26,10 @@ type IPGeo struct {
 	City     string
 
 	ISP string
+	AS  string
+
+	Lat float64
+	Lon float64
 }
 
 func (geo *IPGeo) String() string {
@@ -48,23 +54,39 @@ func GetGeo(ipStr string) (*IPGeo, error) {
 	if ip == nil {
 		return nil, ErrInvalidIP
 	}
-	res, err := httpGet("http://freeapi.ipip.net/" + ipStr)
+	url := fmt.Sprintf("https://btapi.ipip.net/host/info?lang=CN&ip=%s&host", ipStr)
+	res, err := httpGet(url)
 	if err != nil {
 		return nil, err
 	}
 	defer res.Body.Close()
-	resp := make([]string, responseLen)
+	resp := make(map[string]interface{})
 	if err := json.NewDecoder(res.Body).Decode(&resp); err != nil {
 		return nil, ErrDecode
 	}
-	if len(resp) != responseLen {
+	_as, ok := resp["as"]
+	if !ok {
 		return nil, ErrDecode
 	}
+	_area, ok := resp["area"]
+	if !ok {
+		return nil, ErrDecode
+	}
+	area := _area.(string)
+	ls := strings.Split(area, "\t")
+	if len(ls) != responseLen {
+		return nil, ErrDecode
+	}
+	lat, _ := strconv.ParseFloat(ls[5], 64)
+	lon, _ := strconv.ParseFloat(ls[6], 64)
 	return &IPGeo{
+		AS:       _as.(string),
 		IP:       &ip,
-		Country:  resp[0],
-		Province: resp[1],
-		City:     resp[2],
-		ISP:      resp[4],
+		Country:  ls[0],
+		Province: ls[1],
+		City:     ls[2],
+		ISP:      ls[4],
+		Lat:      lat,
+		Lon:      lon,
 	}, nil
 }
